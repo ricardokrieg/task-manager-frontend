23a2a9b0b836fbb5d756e582a8197e68f5573c4a
Ricardo Andrade <ricardo.krieg@gmail.com>
2023-10-20 10:58:35 -0300

    Toptal PR as line chart

--- src/Stats/ToptalPR.js
+++ src/Stats/ToptalPR.js
@@ -1,17 +1,49 @@
-import {Container, ProgressBar, Row} from 'react-bootstrap';
+import React from 'react';
+import {Line} from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import {COLOR_OTHER, COLOR_TOPTAL} from '../colors';
+import {DateTime} from 'luxon';
 import {useQuery} from '@apollo/client';
 import {GET_COMPLETED_TASKS} from '../queries';
 import {includes} from 'lodash';
-import {DateTime} from 'luxon';
-import {COLOR_STRAKE, COLOR_TOPTAL} from '../colors';
 
-const barWidth = (tasks) => {
-  return (tasks.length / 20) * 100;
-};
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const buildDates = (startDate, endDate) => {
+  const dates = [];
+  let date = startDate;
+
+  while (date <= endDate) {
+    if (date.weekday <= 5) {
+      dates.push(date);
+    }
+
+    date = date.plus({ days: 1 });
+  }
+
+  return dates;
+}
 
 export default function ToptalPR() {
   const { loading, error, data } = useQuery(GET_COMPLETED_TASKS);
-  const now = DateTime.now();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
@@ -20,21 +52,43 @@ export default function ToptalPR() {
     .completedTasks
     .filter((t) => includes(t.tags, 'toptal') && includes(t.tags, 'pr'))
     .map((t) => ({...t, timestamp: DateTime.fromISO(t.completedAt)}));
-  const month = tasks.filter((t) => t.timestamp.hasSame(now, 'month') && !t.timestamp.hasSame(now, 'day'));
-  const today = tasks.filter((t) => t.timestamp.hasSame(now, 'day'));
 
-  return (
-    <Container>
-      <Row>
-        <h3>Toptal's External PRs</h3>
+  const startDate = DateTime.now().startOf('month');
+  const endDate = DateTime.now().endOf('month');
+  const dates = buildDates(startDate, endDate);
 
-        <div>
-          <ProgressBar>
-            <ProgressBar now={barWidth(month)} min={0} max={100} style={{ backgroundColor: COLOR_TOPTAL }} />
-            <ProgressBar now={barWidth(today)} min={0} max={100} style={{ backgroundColor: COLOR_STRAKE }} />
-          </ProgressBar>
-        </div>
-      </Row>
-    </Container>
-  );
+  const chartData = {
+    labels: Array.from({ length: dates.length }, () => ''),
+    datasets: [
+      {
+        label: 'Actual',
+        data: dates.filter((date) => date <= DateTime.now()).map((date) => tasks.reduce((acc, t) => t.timestamp >= startDate && t.timestamp <= date ? acc + 1 : acc, 0)),
+        borderColor: COLOR_TOPTAL,
+        backgroundColor: COLOR_TOPTAL,
+      },
+      {
+        label: 'Target',
+        data: Array.from({ length: dates.length }, (_, i) => i + 1),
+        borderColor: COLOR_OTHER,
+        backgroundColor: COLOR_OTHER,
+      },
+    ],
+  };
+
+  return <Line
+    data={chartData}
+    options={{
+      responsive: true,
+      pointRadius: 1,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Toptal External PRs',
+        },
+      },
+    }}
+  />
 }
